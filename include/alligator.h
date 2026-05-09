572d6b6357e60b87565ab55c5c08bc07a4f7ba1f
Niharika Khare <niharikakhare2101@gmail.com>
2026-05-09 22:41:10 +0530

    mm_alloc method in alligator

--- include/alligator.h
+++ include/alligator.h
@@ -11,15 +11,16 @@
 
 
 /** Different slab size to save from over allocation */
-#define SLAB_SIZE_SMALL             PAGE_SIZE - H_SIZE
-#define SLAB_SIZE_MEDIUM            1024 * PAGE_SIZE - H_SIZE
-#define SLAB_SIZE_LARGE             1024 * 1024 * PAGE_SIZE - H_SIZE
+#define SLAB_SIZE_SML               PAGE_SIZE - H_SIZE
+#define SLAB_SIZE_MID               1024 * PAGE_SIZE - H_SIZE
+#define SLAB_SIZE_LRG               1024 * 1024 * PAGE_SIZE - H_SIZE
 
 
 /** Flags bits */
 #define IS_FREE                     1
-#define IS_FIRST_BLOCK              2       // Starting block on slab
-#define IS_LAST_BLOCK               4       // Ending block on slab
+#define IS_FRST_BLK                 2       // Starting block on slab
+#define IS_LAST_BLK                 4       // Ending block on slab
+#define IS_HUGE_BLK                 8
 
 
 typedef unsigned long _align;
@@ -27,18 +28,19 @@ typedef unsigned long _align;
 
 typedef union header {
     struct {
-        unsigned int size;
         unsigned int flags;
+        size_t size;
+        size_t prev_size;
         union header * next;
-        union header * prev;
+        union header * prev;                // points to the last block 
     } head;
     _align _al;
 } _Header;
 
 
 
-void * bite (unsigned int size) ;
-void   drop (void * mem)  ;
+void * mm_alloc (size_t size) ;
+void   mm_free (void * mem)  ;
 
 
 #endif /* _ALLIGATOR_H_ */
--- src/alligator.c
+++ src/alligator.c
@@ -1,25 +1,144 @@
 #include "alligator.h"
 
-_Header * slab_list_1 = NULL;
-_Header * slab_list_2 = NULL;
-_Header * slab_list_3 = NULL;
+#include <stdio.h>
 
-_Header * free_list_small = NULL;
-_Header * free_list_medium = NULL;
-_Header * free_list_large = NULL;
-
-static void _init () {
+_Header * fl_sml = NULL;
+_Header * fl_mid = NULL;
+_Header * fl_lrg = NULL;
 
 
+
+static _Header * _fl_add (_Header * fl, _Header * slab) {
+
+    if (!fl) {
+        return slab;
+    }
+
+    slab->head.next = fl;
+    fl->head.prev = slab;
+    fl->head.prev_size = slab->head.size;
+
+    return slab;
 }
 
+static _Header * _find_f_blk (_Header * fl, size_t size) {
 
-void * bite ( unsigned int t ) {
+    _Header * st = fl;
+
+    do {
+        if (st->head.size == size) {
+
+            if (st->head.next) {
+                st->head.next->head.prev = st->head.prev;
+                st->head.next->head.prev_size = st->head.prev ? st->head.prev->head.size : 0;
+            }            
+
+            if (st->head.prev) {
+                st->head.prev->head.next = st->head.next;
+            }
+            
+            st->head.flags = st->head.flags & !IS_FREE;
+            
+            return st;
+        }
+        else if (st->head.size >= size + H_SIZE) {
+
+            st->head.size = st->head.size - (size + H_SIZE) ;
+            _Header * f_blk = ((_Header *) ((void *) (st + 1 + size) ));
+
+            printf("\nsize: %d\n", size);
+            printf("\ndiff: %d\n", f_blk - st);
+            printf("\ndiff (void *): %d\n", (void *) (f_blk - st));
+            f_blk->head.size = size;
+            f_blk->head.prev_size = st->head.size;
+            f_blk->head.prev = st;
+            f_blk->head.next = st->head.next;
+            f_blk->head.flags = 0;
+                
+            return f_blk;
+        }
+    }
+    while (st != fl);
+
+    return NULL;
+}
+
+static _Header * _mmap (size_t size, int flags) {
+
+    size_t tt_size = size + sizeof (_Header);
+
+    _Header * slab = mmap (NULL, tt_size, PROT_READ | PROT_WRITE,
+                           MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+
+    if (slab == MAP_FAILED) {
+        return NULL;
+    }
     
-    return (void *) -1;
+    slab->head.flags        = flags;
+    slab->head.size         = size;
+    slab->head.prev_size    = 0;
+    slab->head.prev         = NULL;
+    slab->head.next         = NULL;
+
+    return slab;
+
+}
+
+void * mm_alloc (size_t size) {
+
+    unsigned int init_flags = IS_FREE | IS_FRST_BLK | IS_LAST_BLK;
+    _Header * fl;
+    _Header * f_blk = NULL;
+
+    if ( size <= SLAB_SIZE_SML ) {
+
+        if (!fl_sml || !(f_blk = _find_f_blk(fl_sml, size)) ) {
+
+            f_blk = _mmap (SLAB_SIZE_SML, init_flags);
+            fl_sml = !fl_sml ? f_blk : _fl_add(fl_sml, f_blk);
+        }  
+        fl = fl_sml;
+    } 
+    else if (size <= SLAB_SIZE_MID ) {
+
+        if (!fl_mid || !(f_blk = _find_f_blk(fl_mid, size)) ) {
+
+            f_blk = _mmap (SLAB_SIZE_MID, init_flags);
+            fl_mid = !fl_mid ? f_blk : _fl_add(fl_mid, f_blk);
+        }  
+        fl = fl_mid;
+    }
+    else if (size <= SLAB_SIZE_LRG ) {
+
+        if (!fl_lrg || !(f_blk = _find_f_blk(fl_lrg, size)) ) {
+
+            f_blk = _mmap (SLAB_SIZE_LRG, init_flags);
+            fl_lrg = !fl_lrg ? f_blk : _fl_add(fl_lrg, f_blk);
+        } 
+
+        fl = fl_lrg;
+    } 
+    else {
+
+        f_blk = _mmap(size, IS_HUGE_BLK);
+        return f_blk ? (void *) (f_blk + 1) : NULL;
+    }
+
+    if (!f_blk) {
+        f_blk = _find_f_blk(fl, size);
+    }
+    
+
+    return f_blk ? (void *) (f_blk + 1) : NULL;
 }
 
 
-void drop ( void * mem ) {
+void mm_free ( void * mem ) {
+
+    _Header * blk = ((_Header *) mem - 1);
+
+    if (blk->head.flags & IS_HUGE_BLK) {
+        munmap(blk, blk->head.size + H_SIZE);
+    }
 
 }
