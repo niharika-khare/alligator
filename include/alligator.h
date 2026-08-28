3346a82bf69fc96a4d0b37f8187ba35943f223c3
Niharika Khare <niharikakhare2101@gmail.com>
2026-08-28 18:16:02 +0530

    Standardize API contract for mimalloc benchmarking

--- include/alligator.h
+++ include/alligator.h
@@ -4,9 +4,9 @@
 #include <stdio.h>
 
 
-void * mm_alloc (size_t size) ;
-void * mm_realloc (void * mem, size_t size) ;
-void   mm_free (void * mem)  ;
+void * malloc (size_t size) ;
+void * realloc (void * mem, size_t size) ;
+void   free (void * mem)  ;
 
 
 #endif /* _ALLIGATOR_H_ */
